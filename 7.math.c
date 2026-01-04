93b02f4d37bb832485554565613c6d205bb804f6
Davii-Ng <nminhd98@gmail.com>
2026-01-04 15:54:01 -0500

    math

---
+++ 7.math.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+    int x = 9;
+    float y = 3.14;
+
+
+    //x = sqrt(x);
+    //x = pow(x, 3);
+    // x = abs(x);
+    // x = log(x);
+    y = round(y);
+
+    printf("%d\n", x);
+    printf("%f", y);
+    return 0;
+}
